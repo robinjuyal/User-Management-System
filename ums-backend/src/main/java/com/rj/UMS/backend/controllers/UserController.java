2f866c4c68377564411e20728f265193eab11808
robinjuyal <85496825+robinjuyal@users.noreply.github.com>
2025-01-16 12:53:27 +0530

    saving and updating user feature

--- ums-backend/src/main/java/com/rj/UMS/backend/controllers/UserController.java
+++ ums-backend/src/main/java/com/rj/UMS/backend/controllers/UserController.java
@@ -22,5 +22,30 @@ import com.rj.UMS.backend.services.UserService;
 @RequestMapping("api/users")
 public class UserController {
 
-   
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public ResponseEntity<?> createUser(@RequestBody UserDto userDto) throws Exception {
+
+        Boolean saveUser = userService.createUser(userDto);
+
+        if (saveUser) {
+            return new ResponseEntity<>("saved successfully", HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>("Failed to save", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable String id, @RequestBody UserDto userDto) throws Exception {
+
+        Boolean saveUser = userService.updateUser(id, userDto);
+
+        if (saveUser) {
+            return new ResponseEntity<>("Updated successfully", HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>("Failed to update", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
 }
